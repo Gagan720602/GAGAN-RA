31b5fe6022175b37f4d5d22c96b796c6b09b1d85
Gagan720602 <76946122+Gagan720602@users.noreply.github.com>
2021-03-09 18:10:55 +0530

    Hcf of 2 numbers

---
+++ Gmail/src/com/gmail/inbox/Gcd.java
@@ -0,0 +1,29 @@
+package com.gmail.inbox;
+
+import java.util.Scanner;
+
+public class Gcd {
+	public static int test(int x,int z)
+	{
+		int gcd=0;
+		for(int i=1;i<=x &&i<=z;i++)
+		{
+			if(x%i==0&&z%i==0)
+			{
+				gcd=i;
+				
+			}
+		}
+		return gcd;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		
+		
+ System.out.println( test(s.nextInt(),s.nextInt()));
+	}
+
+
+}
