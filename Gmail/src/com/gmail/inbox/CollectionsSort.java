83e19a28b4e31349d19f0bad8ee6e65b58127ec3
Gagan720602 <76946122+Gagan720602@users.noreply.github.com>
2021-03-27 01:06:52 +0530

    Collection sorts techniques

--- Gmail/src/com/gmail/inbox/CollectionsSort.java
+++ Gmail/src/com/gmail/inbox/CollectionsSort.java
@@ -20,17 +20,19 @@ public class CollectionsSort {
 		list.add(sc.nextInt());
 		
      Collections.sort(list);
-     
+    
+     System.out.println("the min of list is= "+Collections.min(list));//return minimum in collection list
      System.out.println("the max of list is= "+Collections.max(list));//return maximum in collection list
      
-     System.out.println("the min int list is= "+Collections.min(list));//return minimum in collection list
-     
    //  Collections.reverse(l);//prints the list in Decending order
+     Collections.sort(list);
+     System.out.println("Accending order"+list);
+     
+     Collections.reverse(list);
+     System.out.println("Decending order"+list);
      
      Collections.shuffle(list);//shuffle the list randomly
-     
-    
-     System.out.println(list);
+     System.out.println("shuffed list"+list);
 	}
 
 
