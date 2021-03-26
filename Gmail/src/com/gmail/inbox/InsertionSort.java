babd7aaa5cf7cee3b88381d21cd2adaa7bad4a98
Gagan720602 <76946122+Gagan720602@users.noreply.github.com>
2021-03-26 21:26:18 +0530

    Insertion sort

---
+++ Gmail/src/com/gmail/inbox/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.gmail.inbox;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+	static void insort(int x[])
+	{
+		for(int i=1;i<x.length;i++)
+		{
+			int key=x[i];
+			int j=i-1;
+					while(j>=0 && x[j]>key)
+					{
+						x[j+1]=x[j];
+						j--;
+					}
+					x[j+1]=key;
+					
+		}
+		System.out.println(Arrays.toString(x));
+		
+	}
+
+	public static void main(String[] args) {
+		int []s= {12,11,13,5,6,50,0};
+        insort(s);
+	}
+
+}
