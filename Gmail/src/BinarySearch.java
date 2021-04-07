3eb83a2d6ab524b2920e3e6fa82014f3db352c87
Gagan720602 <76946122+Gagan720602@users.noreply.github.com>
2021-04-07 12:43:26 +0530

    Binary Search

---
+++ Gmail/src/BinarySearch.java
@@ -0,0 +1,37 @@
+
+public class BinarySearch {
+	public static boolean test(int []a,int key)
+	{
+		int low=0;
+		int high=a.length-1;
+		int mid;
+		for(int i=0;i<a.length;i++)
+		{
+			mid=(low+high)/2;
+			
+			if(key==a[mid])
+			{
+				return true;
+			}
+			else if(key>a[mid])
+			{
+				low=mid+1;
+			}
+			else
+			{
+				high=mid-1;
+			}
+			
+		}
+		return false;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6};
+	System.out.println(test(a,5));
+
+	}
+
+}
