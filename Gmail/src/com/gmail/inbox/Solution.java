331265f5c25f662002a6aa2504b74dd825b51dab
Gagan720602 <76946122+Gagan720602@users.noreply.github.com>
2021-06-30 03:12:02 +0530

    Longest palindromesubstring

---
+++ Gmail/src/com/gmail/inbox/Solution.java
@@ -0,0 +1,54 @@
+package com.gmail.inbox;
+
+class Peace{
+	
+	  
+    public static  int expandFromMiddle(String s, int left, int right)
+    {
+        if(s == null || left > right){
+            return 0;
+        }
+        while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right))
+        {
+            left--;
+            right++;
+        }
+        return right-left-1;
+    }
+
+}
+
+class Solution {
+	
+	
+	static Peace p= new Peace();
+	
+	
+	public static void main(String []args) {
+		
+	 String s= "babad";
+	       
+        if(s == null || s.length() < 1)
+        {
+           
+        }
+        int start = 0;
+        int end =0;
+        for(int i=0; i<s.length(); i++)
+        {
+            int len1 = p.expandFromMiddle(s, i, i);
+            int len2 = p.expandFromMiddle(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if(len > end-start)
+            {
+                start = i - ((len-1)/2);
+                
+                end = i + (len/2);
+            }
+        }
+       System.out.println( s.substring(start, end+1));
+    
+        
+      
+}
+}
